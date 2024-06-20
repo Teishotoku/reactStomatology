74bc825bbc64f40a9a22701724f26bd29d20a433
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2024-06-21 00:24:00 +0200

    start about

--- src/pages/footer/page.jsx
+++ src/pages/footer/page.jsx
@@ -1,15 +1,46 @@
 import "./style/style.scss";
+
 import logo from "./style/img/logo-black.png";
+import phone from "./style/icon/telephone.svg";
+import mail from "./style/icon/mail.svg";
+import facebook from "./style/icon/facebook.svg";
+import instagram from "./style/icon/instagram.svg";
+import telegram from "./style/icon/telegram.svg";
+import house from "./style/icon/house.svg";
 
 const FOOTER = () => {
     return(
         <footer>
-					<div className="footer-left"></div>
+					<div className="footer-block">
+						<div className="leftBlock">
+							<img src={phone}/>
+							<p>34-234-982</p>
+						</div>
+						<hr/>
+						<div className="leftBlock">
+							<img src={mail}/>
+							<p>starTist@gmail.com</p>
+						</div>
+			</div>
+
 					<div className="footer-center">
 						<img src={logo} alt=""/>	
 						<p>STARTIST</p>
 					</div>
-					<div className="footer-right"></div>
+
+					<div className="footer-block">
+						<div className="footerSocial">
+							<img src={instagram}/>
+							<img src={facebook}/>
+							<img src={telegram}/>
+						</div>
+						<hr/>
+						<div className="footerAdress">
+							<p>leak street 23b</p>
+							<img src={house}/>
+						</div>
+					</div>
+
 				</footer>
     );
 }
