b8babb5bc4e833fa90922e6d3a897d5aa121d521
Teishotoku <Dl1SqP1L4F0he@protonmail.com>
2024-06-20 02:01:09 +0200

    main full

--- src/pages/footer/page.jsx
+++ src/pages/footer/page.jsx
@@ -1,6 +1,16 @@
+import "./style/style.scss";
+import logo from "./style/img/logo-black.png";
+
 const FOOTER = () => {
     return(
-        <footer></footer>
+        <footer>
+					<div className="footer-left"></div>
+					<div className="footer-center">
+						<img src={logo} alt=""/>	
+						<p>STARTIST</p>
+					</div>
+					<div className="footer-right"></div>
+				</footer>
     );
 }
 
--- src/pages/main/comp/links.jsx
+++ src/pages/main/comp/links.jsx
@@ -1,7 +1,42 @@
+import pills from "../style/img/Pills-background.png";
+
 const LinksMain = () => {
     return (
         <section className="LinksMain">
-            <div></div>
+            <div className="centering">
+							<img src={pills} alt=""/>		
+							<div className="layer">
+								<div className="upper">
+
+									<div className="paragraph">
+										<div className="round"><div className="inside"/></div>
+										<p>500 <span>400</span> dollars of operation in this 5 days</p>
+								  </div>
+
+								</div>
+
+								<div className="downer">
+									<div className="downer-l">
+
+										<div className="paragraph">
+											<div className="round"><div className="inside"/></div>		
+											<p>new our clinic implants from Germany</p>
+										</div>
+
+										<div className="paragraph">
+											<div className="round"><div className="inside"/></div>		
+											<p>Warranty and service <span>on 1 year</span></p>
+										</div>
+
+								</div>
+
+									<div className="downer-r">
+										<button>SIGN RIGHT NOW</button>
+									</div>
+								</div>
+
+						  </div>
+						</div>
         </section>
     );
 
